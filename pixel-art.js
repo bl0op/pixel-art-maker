2b15ce1a6dd88083b2dba7a3000f45b1e8dfa112
bloop <jaroslavkalinin97@gmail.com>
2019-12-12 22:40:45 +0300

    Canvas fixed, added 2-color pallete

--- pixel-art.js
+++ pixel-art.js
@@ -2,15 +2,18 @@
 ///@date: 03.12.19
 ///@brief: script for pixel-art maker tool
 
-//this function variable we will use to switch tool: pen, erase and other.
+//this function variable is using to switch tool: pen, erase and other.
 var toolFunction = function (event) {
 }
 
+//current color
+var currentColor = "red";
+
 //pen tool (Captain obvious: color selected pixel)
 function pen(color, pixel){
-    pixel.setAttribute("style", "background-color: "+color+";");
+    pixel.style.backgroundColor = color;
+    pixel.style.border = "0px";
 }
-
 
 function createCanvas(width, height){
     canvas = document.querySelector("#canvas");
@@ -25,6 +28,21 @@ function createCanvas(width, height){
         for(let column_num = 0; column_num < width; column_num++){
             pixel = document.createElement("div");
             pixel.className = "pixel";
+            //if it is left ramp pixel don't show left border
+            if(column_num == 0){
+                pixel.style.borderLeft = '0px';
+            }
+            //and so on...
+            if(column_num == width-1){
+                pixel.style.borderRight = '0px'
+            }
+            if(row_num == 0){
+                pixel.style.borderTop = '0px'
+            }
+            if(row_num == height-1){
+                pixel.style.borderBottom = '0px'
+            }
+
             pixel.addEventListener('click', toolFunction);
             row.appendChild(pixel);
         }
@@ -32,11 +50,39 @@ function createCanvas(width, height){
     }
 }
 
+function createPallete(){
+    var colors_count = 2;
+    pallete = document.querySelector('#pallete');
+    //clear pallete
+    while(pallete.firstChild){
+        pallete.removeChild(pallete.firstChild);
+    }
+    color_row = document.createElement("div");
+    color_row.className = "color-row";
+    /*for(let color_num = 0; color_num < colors_count; color_num++){
+        color_cell = document.createElement("div");        
+        color_cell.className = "color-cell";
+        color_row.appendChild(color_cell);
+        }*/
+        color_cell = document.createElement("div");        
+        color_cell.style.backgroundColor = "white";
+        color_cell.className = "color-cell";
+        color_cell.addEventListener("click", function(event){ currentColor = event.target.style.backgroundColor;});
+        color_row.appendChild(color_cell);
+        color_cell = document.createElement("div");        
+        color_cell.style.backgroundColor = "red";
+        color_cell.className = "color-cell";
+        color_cell.addEventListener("click", function(event){ currentColor = event.target.style.backgroundColor;});
+        color_row.appendChild(color_cell);
+    pallete.appendChild(color_row);
+}
+
 //change to pen
 toolFunction = function(event){
-    pen("rgb(255,0,0)", event.target);
+    pen(currentColor, event.target);
 }
 
 
 
-createCanvas(64,64);
+createCanvas(64,32);
+createPallete();
